9e5b985df928ea9345129079e935a39536a914c2
DizArt-sis <114420890+DizArt-sis@users.noreply.github.com>
2025-06-21 18:22:20 +1000

    Delete info.php

--- info.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $link = mysqli_connect('db','root','Test123');
-    if (!$link) {
-        die('Error:'. mysqli_connect_error());
-    }
-    echo "Good!";
-    mysqli_close($link);
-
-?>
