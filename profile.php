3c85034957e3d895ba6a255bb83b2e10928cfe66
DizArt-sis <114420890+DizArt-sis@users.noreply.github.com>
2025-06-21 16:01:49 +1000

    Update profile.php

--- profile.php
+++ profile.php
@@ -65,7 +65,7 @@
         exit();
     }
     require_once('db.php');
-    $link=mysqli_connect('127.0.0.1','root','kali','first');
+    $link=mysqli_connect('db','root','Test123','first');
 
     if (isset($_POST['submit'])) {
         $title = $_POST['postTitle'];
